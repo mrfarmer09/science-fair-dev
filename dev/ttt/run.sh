927ae099988c54c66bb56cc3deb96cb116f44792
Mr Farmer <asher@ourjohnsonfamily.net>
2023-11-22 10:14:26 -0600

    Changed design of program

--- dev/ttt/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true; do
-    clear
-    python3 main.py
-    read -p "Press Enter to run again, or Ctrl+C to exit..."
-done
--- docs/index.md
+++
@@ -1,3 +0,0 @@
-# Update
-I have moved to using a basic minimax algorithm.
-I will update the documentation when I have the time.
